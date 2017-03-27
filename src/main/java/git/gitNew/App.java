5c2f0c56c87f1e88b73d6da19adceda0725ec49b
pinky <pinky.udeg@synerzip.com>
2017-03-27 14:50:23 +0530

    first commit

---
+++ src/main/java/git/gitNew/App.java
@@ -0,0 +1,13 @@
+package git.gitNew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
